f06243cf6a0c036f3b0bd69a8965dc64f7933177
Ryujin Shin <deverajoshua02@gmail.com>
2022-09-08 21:00:43 +0800

    added logout function to admin dashboard and also fixes bugs on controller where in the users my access the admin dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends Controller
     protected function authenticated ()
     {
         if(Auth::user()->role_as == '1'){
-            return redirect('admin/dashboard')->with('status', 'Welcome back admin');
+            return redirect('admin/dashboard')->with('message', 'Welcome back');
         }
         else{
             return redirect('/home')->with('status', 'Logged in Successfully');
