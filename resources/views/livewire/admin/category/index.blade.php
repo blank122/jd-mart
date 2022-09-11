1720183ad63a992030212de186a77aaedfff2487
Ryujin Shin <deverajoshua02@gmail.com>
2022-09-11 20:00:33 +0800

    added table to fetch the data in the database

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,21 +1,47 @@
+{{-- If your happiness depends on money, you will never be happy with yourself. --}}
 
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card-header">
-                @if(session()->has('message'))
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-header">
+            @if (session()->has('message'))
                 <div class="alert alert-success">
-                    {{session()-> get('message')}}
+                    {{ session()->get('message') }}
                 </div>
-                @endif
-    
-                <h3>Category
-                    <a href="{{ url('admin/category/create') }}" class ="btn btn-outline-success btn-sm float-end">Add Category</a> {{--add category--}}
-                </h3>
-            </div>
-            <div class="card-body bg-gray">
-                <h1>hello</h1>
+            @endif
+
+            <h3>Category
+                <a href="{{ url('admin/category/create') }}" class="btn btn-outline-success btn-sm float-end">Add
+                    Category</a> {{-- add category --}}
+            </h3>
+        </div>
+        <div class="card-body bg-gray">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category_item)
+                        <tr>
+                            <td>{{ $category_item->id }}</td>
+                            <td>{{ $category_item->name }}</td>
+                            <td>{{ $category_item->status == '1' ? 'Hidden' : 'Visible' }}</td>
+                            <td>
+                                <a href="" class="btn btn-success">Edit</a>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div>
+                {{ $categories->links() }}
             </div>
         </div>
     </div>
+</div>
