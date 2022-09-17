c9490b3e14cce4b621c3d9aa3e5567422317c248
Ryujin Shin <deverajoshua02@gmail.com>
2022-09-18 07:17:22 +0800

    product branch

---
+++ app/Http/Livewire/Admin/ProductBrand/ProductBrand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\ProductBrand;
+
+use Livewire\Component;
+
+class ProductBrand extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.product-brand.product-brand');
+    }
+}
